ae00408c7045eb17c8bad5b8152a5e7d915e4e9e
kin <cheongkinpui@163.com>
2015-12-17 11:34:02 +0800

    add index.php descriotion

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <?php
+/**
+ *
+ * 这是创宇数码的售后管理系统
+ * 系统的开发者: 张剑培
+ * 我的联系方式: 15916357625
+ * 系统开发日期: 2015-12-17
+ * 
+ * MVC开发模式
+ * smaty 模板
+ * 前端技术包含
+ *  HTML5
+ *  CSS3
+ *  JavaScript
+ *
+ * 后端技术包括
+ *  PHP
+ *
+ * 数据库使用
+ *  Mysql
+ *
+ *
+ **/
+header("Content-type: text/html; charset=utf-8"); //设置编码
+
 /*
  *引入function 方法
  **/
-header("Content-type: text/html; charset=utf-8"); //设置编码
 require_once("function.php");
-$c = in_array($_GET['c'],$controllerAllow)?filter($_GET['c']):'index';	//对浏览器传递来的字符串进行转义
+
+//对浏览器传递来的字符串进行转义
+$c = in_array($_GET['c'],$controllerAllow)?filter($_GET['c']):'index';	
 $m = in_array($_GET['m'],$methodAllow)?filter($_GET['m']):'index';
+
 $smarty = new smarty();
 /*
  *smarty 配置
@@ -22,8 +48,8 @@ $smarty->template_dir = "tpl";
 $smarty->compile_dir = "template_c";
 $smarty->cache_dir = "cache";
 
-//开启缓存
-$smarty->caching = true;
+//关闭(开启)缓存
+$smarty->caching = false;
 
 //设置缓存时间
 $smarty->cache_lifetime = 120;
