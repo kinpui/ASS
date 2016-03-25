00c45c4978c9a6efcec5ab2e7dad941370f972c2
kinpui <cheongkinpui@163.com>
2016-03-25 12:21:18 +0800

    add codeigniter

--- Lib/Controller/loginController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class loginController{
-
-  function login(){
-    V::assign ( array( 'login_title'=>'login' ) );
-    V::display( 'login.html' );
-  }
-
-}
--- README.md
+++
@@ -1,6 +0,0 @@
-# ASS
-
-######（ After sale system ）
-chuangyu Digital Co., Ltd. 售后系统
-
-#售后系统流程
